bbf141fec0b2b3a81a4e4d2a2b751b0e7792b8cc
Elias <eliascamilo70@outlook.com>
2024-11-15 18:27:50 -0300

    radio and button play

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
     import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
     import DiaryPage from './pages/DiaryPage';
     import MoviesPage from './pages/MoviesPage';
@@ -7,12 +7,28 @@ import React from 'react';
     import './index.css';
 
     const App = () => {
+      const [audio] = useState(new Audio('https://streaming.radio.co/sefac315e7/listen'));
+      const [playing, setPlaying] = useState(false);
+
+      useEffect(() => {
+        playing ? audio.play() : audio.pause();
+      }, [playing, audio]);
+
+      useEffect(() => {
+        audio.addEventListener('ended', () => setPlaying(false));
+        return () => {
+          audio.removeEventListener('ended', () => setPlaying(false));
+        };
+      }, [audio]);
+
+      const togglePlay = () => setPlaying(!playing);
+
       return (
         <Router>
           <div className="container">
             <div className="navbar">
               <Link to="/">Home</Link>
-              <Link to="/diary">Diário</Link>
+              <Link to="/diary">DIário</Link>
               <Link to="/movies">Filmes</Link>
               <Link to="/music">Música</Link>
             </div>
@@ -20,8 +36,9 @@ import React from 'react';
               <span>We are online! 🌸🎀</span>
             </div>
             {/* <div className="gif-container">
-            
+             
             </div> */}
+            <button onClick={togglePlay}>{playing ? 'Pause Sound' : 'Play Sound'}</button>
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/diary" element={<DiaryPage />} />
