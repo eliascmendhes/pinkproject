a0a29f8f102490b365028c9e6546c6d8571e0b23
Elias <eliascamilo70@outlook.com>
2024-11-15 21:24:10 -0300

    new add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
     const HomePage = () => {
       return (
         <div>
           <div className="category">
             <h2>Diário</h2>
             <div className="entry-block">
-              <p>Site em desenvolvimento. Quero encontrar uma estética dos anos 2000. coisa velha. internet antiga. estive vendo muitos sites retro. </p>
+              <p>Site em desenvolvimento. </p>
             </div>
           </div>
           <div className="category">
             <h2>Filmes</h2>
             <div className="entry-block">
-              <p></p>
+            <p>9ª MOSTRA DE CINEMA CHINÊS: LONGE, MAS PERTO</p>
+            <a href="https://www.spcineplay.com.br/pages/2" target="_blank" rel="noopener noreferrer">link</a>
             </div>
           </div>
           <div className="category">
             <h2>Música</h2>
             <div className="entry-block">
-              <p></p>
+              <p>Aperta o play. Eu gosto dessa rádio</p>
             </div>
           </div>
           
